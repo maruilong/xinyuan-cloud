43c6c3491d9492d8d0eb40fb375d10ff5ea9b3c0
maruilong <774328384@qq.com>
2018-12-11 23:45:23 +0800

    first commit

---
+++ xinyuan-eureka/src/main/java/com/xinyuan/eureka/CloudEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.xinyuan.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shinian
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
